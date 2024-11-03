15953483aa599c65a2d9e819647e98eb1e1d9557
ph-3 <aaprashant2005@gmail.com>
2024-11-03 13:19:54 +0530

    issue resolve

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import Header from './comp/Header'
-import Footer from './comp/footer'
+import Footer from './comp/Footer'
 import Main from './comp/main'
 function App() {
 
